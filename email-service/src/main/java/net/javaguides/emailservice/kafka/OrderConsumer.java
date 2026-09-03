b13a03938ef65d3dec345c51eb380a9bb4413a50
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-03 12:25:25 +0200

    309. EmailService Microservice - Configure and Create Kafka Consumer

---
+++ email-service/src/main/java/net/javaguides/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,30 @@
+package net.javaguides.emailservice.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javaguides.basedomains.dto.OrderEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * OrderConsumer
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 02/09/2026 - 19:27
+ * @since 1.17
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderConsumer {
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(final OrderEvent orderEvent) {
+        log.info(String.format("Order event received in email service => %s", orderEvent));
+
+        // send email notification to the customer or perform any other necessary actions
+    }
+
+}
